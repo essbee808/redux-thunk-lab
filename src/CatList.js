d202a7f8f628d0f9f55b80af16cc6f79993d035d
Sheena Bonilla <sheena.myb@gmail.com>
2022-08-30 22:06:34 -0700

    commit tree 384f1fb9442152b5c876c497710a3fd012e6c69f

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,23 @@
 // write your CatList component here
+import React from 'react';
+
+class CatList extends React.Component {
+
+    renderList = () => {
+        return (
+        this.props.catPics.map((pic) => 
+         <img key={pic.id} src={pic.url} alt="cat"></img>
+         )
+      )
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
+
+export default CatList;
--- src/reducers/catsReducer.js
+++ src/reducers/catsReducer.js
@@ -1,1 +1,25 @@
 // add the catsReducer
+const catsReducer = (state = {cats: [], loading: false}, action) => {
+
+    switch (action.type) {
+        case "LOADING_CATS":
+            return {
+                ...state, 
+                cats: [...state.cats],
+                loading: true,
+            };
+        
+        // hint: expecting a payload object with a cats key
+        case "ADD_CATS":
+            return {
+                ...state,
+                cats: action.cats,
+                loading: false,
+            };
+        
+        default:
+            return state;
+    }
+}
+
+export default catsReducer;
